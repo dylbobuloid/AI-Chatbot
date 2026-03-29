bb985f870ca2525dc3464c618aac6cfed8d08a1e
Dylan Fernandes <87030593+dylbobuloid@users.noreply.github.com>
2026-03-29 23:48:44 +0100

    message submission creates a user chat log

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,5 +1,22 @@
 
 
 function sendMessage(){
+    let message = document.getElementById("user-input").value
+    console.log("This is your message: ", message)
+    addToLogUser(message)
+
+
+
+}
+
+function addToLogUser(message){
+    
+    const chatBox = document.getElementById("chat-box")
+    const newDiv = document.createElement("div")
+
+    const userText = document.createTextNode("USER --> " + message)
+    newDiv.setAttribute("id", "user-message")
+    newDiv.appendChild(userText)
+    chatBox.appendChild(newDiv)
 
 }
